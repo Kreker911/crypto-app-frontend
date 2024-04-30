39835af80c65832a3ff3f93014d798357f5f75c6
Ilya <honeyflym@gmail.com>
2024-04-30 16:51:36 +0300

    add basic file structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Header from "../Header/Header";
+import PreLoader from "../PreLoader/PreLoader";
+
+export default function App() {
+  return (
+    <>
+      <PreLoader />
+    </>
+  );
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>React Calculator</h1>
+    </div>
+  );
+};
+
+export default Header;
