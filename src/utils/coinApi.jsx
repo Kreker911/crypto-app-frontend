33348e6381e208a0596f6fc51473254bb81706ca
Ilya <honeyflym@gmail.com>
2024-06-09 17:22:53 +0300

    fix media props in  SideBar

--- src/utils/coinApi.jsx
+++ src/utils/coinApi.jsx
@@ -1,6 +1,8 @@
 import { request } from "./api";
 import { apiOptions } from "./constants";
 
+// FETCH for vite
+
 // export const fetchCoins = (currentPage) => {
 //   return request(
 //     `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=100&page=${currentPage}&sparkline=true&price_change_percentage=24h%2C7d`,
@@ -17,6 +19,8 @@ import { apiOptions } from "./constants";
 //     `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${days}`
 //   );
 // };
+
+// FETCH for Vercel
 
 export const fetchCoins = (currentPage) => {
   return request(
